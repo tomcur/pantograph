206f39a283f6432eb2d82bf1f3a350cc4b1e05d0
Thomas Churchman <thomas@churchman.nl>
2026-03-16 00:03:32 +0100

    README

--- pantograph/src/lib.rs
+++ pantograph/src/lib.rs
@@ -1,10 +1,22 @@
 //! `pantograph` provides channels for sending payloads across threads, where at least one side of
 //! the channel is *wait-free*.
 //!
-//! Synchronization being "wait-free" is a stronger guarantee than "lock-free." Lock-free
-//! guarantees the full system makes progress and cannot get in a deadlock; wait-free additionally
-//! guarantees per-thread progress. Wait-free channels are useful for problems that must meet
-//! particular timing requirements, but they are not necessarily faster than lock-free channels.
+//! Synchronization being "wait-free" is a stronger guarantee than "lock-free." Wait-free
+//! operations complete in a bounded number of steps regardless of contention by other threads or
+//! preemption of other threads by the operating system.
+//!
+//! In lock-based synchronization (like `Mutex` from Rust's standard library), if the operating
+//! system preempts a thread that holds a lock, other threads waiting to acquire the lock are
+//! blocked until it resumes.
+//!
+//! Lock-free synchronization does not use locking mechanisms, and the system is guaranteed to make
+//! progress even when some threads are preempted by the operating system. However, individual
+//! operations may fail under contention from other threads and need to be retried, which means no
+//! bound can be given on the number of steps required to complete a lock-free operation.
+//!
+//! Wait-free and lock-free channels are useful for problems that must meet particular timing
+//! requirements or that must be robust against preemption. They are not necessarily faster than
+//! lock-based channels.
 
 #![no_std]
 
